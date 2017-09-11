f0654db6e8e4e83a9f0d645648334661527cc735
vctrferreira <victtorferreiralima@gmail.com>
2017-09-11 11:42:18 -0300

    Fix env name

--- app/models/base_models.py
+++ app/models/base_models.py
@@ -2,7 +2,7 @@ import peewee
 from decouple import config
 
 DATABASE = peewee.PostgresqlDatabase('bothub', user='postgres',
-                                     password='postgres', host=config('BOTHUB_POSTGRIS'))
+                                     password='postgres', host=config('BOTHUB_POSTGRES'))
 
 
 class BaseModel(peewee.Model):
