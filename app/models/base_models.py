8d1148fcf8ae9639ef09fd296393e2abefb64f72
vctrferreira <victtorferreiralima@gmail.com>
2017-09-20 11:00:28 -0300

    Fix pep8

--- app/models/base_models.py
+++ app/models/base_models.py
@@ -1,8 +1,12 @@
 import peewee
 from decouple import config
 
-DATABASE = peewee.PostgresqlDatabase('bothub', user=config('BOTHUB_POSTGRES_USER'),
-                                     password=config('BOTHUB_POSTGRES_PASSWORD'), host=config('BOTHUB_POSTGRES'), port=config('BOTHUB_POSTGRES_PORT'))
+DATABASE = peewee.PostgresqlDatabase(
+    config('BOTHUB_POSTGRES_DB'),
+    user=config('BOTHUB_POSTGRES_USER'),
+    password=config('BOTHUB_POSTGRES_PASSWORD'),
+    host=config('BOTHUB_POSTGRES'),
+    port=config('BOTHUB_POSTGRES_PORT'))
 
 
 class BaseModel(peewee.Model):
