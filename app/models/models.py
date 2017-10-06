b9f7235d05c58df69c9ece8d76e87bab33c5404d
vctrferreira <victtorferreiralima@gmail.com>
2017-10-06 14:27:47 -0300

    Add model string return

--- app/models/models.py
+++ app/models/models.py
@@ -3,12 +3,22 @@ from datetime import datetime
 
 import uuid
 import peewee
+import json
 
 
 class Profile(BaseModel):
     uuid = peewee.UUIDField(primary_key=True, default=uuid.uuid4)
     created_at = peewee.DateTimeField(default=datetime.now)
     updated_at = peewee.DateTimeField()
+
+    def __str__(self):
+        r = {}
+        for k in self._data.keys():
+            try:
+                r[k] = str(getattr(self, k))
+            except:
+                r[k] = json.dumps(getattr(self, k))
+        return str(r)
 
     def save(self, *args, **kwargs):
         self.updated_at = datetime.now()
@@ -29,6 +39,15 @@ class Bot(BaseModel):
     def save(self, *args, **kwargs):
         self.updated_at = datetime.now()
         return super(Bot, self).save(*args, **kwargs)
+
+    def __str__(self):
+        r = {}
+        for k in self._data.keys():
+            try:
+                r[k] = str(getattr(self, k))
+            except:
+                r[k] = json.dumps(getattr(self, k))
+        return str(r)
 
     class Meta:
         db_table = 'bots'
