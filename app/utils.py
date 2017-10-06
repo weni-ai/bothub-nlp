36120fa661cf00951e55a82e3f48179d5e2b3bea
vctrferreira <victtorferreiralima@gmail.com>
2017-10-06 10:36:52 -0300

    Add decorator to require token

--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,16 @@
 INVALID_TOKEN = "invalid_token"
 DB_FAIL = "db_fail"
+
+
+def token_required(f):
+    def check(handler, *args, **kwargs):
+        auth_token = handler.request.headers.get('Authorization')
+        if auth_token and len(auth_token) == 32:
+            return f(handler, *args, **kwargs)
+        else:
+            handler.set_status(401)
+            handler.write("Auth token wrong")
+            handler.finish()
+    check.__doc__= f.__doc__
+    check.__name__= f.__name__
+    return check
