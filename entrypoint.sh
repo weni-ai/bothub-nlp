f2f732bd1ad9a5091a26df535baa070320064cc6
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-11-08 14:44:05 -0300

    Add utf-8 settings which fix click on linux

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 WEBAPP_HOME=/home/app/webapp
 PYTHON_VENV=/home/app/webapp/env
 PYTHON=$PYTHON_VENV/bin/python
+LC_ALL=C.UTF-8
+LANG=C.UTF-8
 git clone $APP_REPOSITORY_URL bothub
 cd bothub
 git checkout $APP_REPOSITORY_BRANCH
