b9da4749eba6f5e8a0d1d44154c34dde92763d21
vctrferreira <victtorferreiralima@gmail.com>
2017-12-20 16:58:26 -0300

    Add permissions support message handler

--- app/handlers/message.py
+++ app/handlers/message.py
@@ -3,13 +3,14 @@ import logging
 import redis
 import cloudpickle
 
-from tornado.web import asynchronous
+from tornado.web import asynchronous, HTTPError
 from tornado.gen import coroutine
-from app.handlers.base import BothubBaseHandler, SPACY_LANGUAGES
+from app.handlers.base import SPACY_LANGUAGES
+from app.handlers.repository_manager import RepositoryManagerHandler
 from app.models.base_models import DATABASE
-from app.models.models import Bot
-from app.settings import DEBUG, REDIS_CONNECTION
-from app.utils import token_required
+from app.models.models import Repository
+from app.settings import DEBUG, REDIS_CONNECTION, ALL_PERMISSIONS
+from app.utils import token_required, INVALID_REPOSITORY
 from rasa_nlu.model import Metadata, Interpreter
 
 
@@ -23,21 +24,28 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-class MessageRequestHandler(BothubBaseHandler):
+class MessageRequestHandler(RepositoryManagerHandler):
     """
     Tornado request handler to predict data.
     """
+    allowed_permissions = ALL_PERMISSIONS
+
+    def _set_bot_on_redis(self, bot_uuid, bot):
+        redis.Redis(connection_pool=REDIS_CONNECTION).set(bot_uuid, bot)
+
     @asynchronous
     @coroutine
     @token_required
     def get(self):
-        auth_token = self.get_cleaned_token()
         bot_uuid = self.get_argument('bot', None)
         message = self.get_argument('msg', None)
+        self._check_repo_user_authorization(bot_uuid)
+
         redis_bot = redis.Redis(connection_pool=REDIS_CONNECTION).get(bot_uuid)
         if redis_bot is not None:  # pragma: no cover
             if DEBUG:
                 logger.info('Reusing from redis...')
+
             redis_bot = cloudpickle.loads(redis_bot)
             bot_language = 'en'
             metadata = Metadata(redis_bot, None)
@@ -48,19 +56,18 @@ class MessageRequestHandler(BothubBaseHandler):
             })
         else:
             if DEBUG:
-                logger.info('Creating a new instance...')
+                logger.info('Using a instance from postgres...')
 
             with DATABASE.execution_context():
-                try:
-                    instance = Bot.get(Bot.uuid == bot_uuid)
+                instance = Repository.select().where(Repository.uuid == bot_uuid).get()
+                bot = cloudpickle.loads(instance.bot)
 
-                    bot = cloudpickle.loads(instance.bot)
-                    bot_language = 'en'
-                    metadata = Metadata(bot, None)
-                    interpreter = Interpreter.load(metadata, {}, SPACY_LANGUAGES[bot_language])
-                    self.write({
-                        'bot': dict(uuid=bot_uuid),
-                        'answer': interpreter.parse(message)
-                    })
-                except:
-                    pass
+            self._set_bot_on_redis(bot_uuid, cloudpickle.dumps(bot))
+
+            bot_language = 'en'
+            metadata = Metadata(bot, None)
+            interpreter = Interpreter.load(metadata, {}, SPACY_LANGUAGES[bot_language])
+            self.write({
+                'bot': dict(uuid=bot_uuid),
+                'answer': interpreter.parse(message)
+            })
