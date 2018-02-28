a67981bef3fc0f10b72b3029509241512880ea58
Douglas Paz <eu@douglaspaz.com>
2018-02-28 16:43:18 -0300

    Update app.utils imports

--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,13 @@
 import uuid
 
-from tornado import web
+from tornado.web import HTTPError
 
 
 def authorization_required(f):
     def check(handler, *args, **kwargs):
         repository_authorization = handler.repository_authorization()
         if not repository_authorization:
-            raise web.HTTPError(status_code=401)
+            raise HTTPError(status_code=401)
         return f(handler, *args, **kwargs)
 
     check.__doc__ = f.__doc__
