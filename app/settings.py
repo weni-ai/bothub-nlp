486691d0d356476b7e6d401f72e3c762416f6913
Douglas Paz <eu@douglaspaz.com>
2018-06-13 16:08:11 -0300

    Remove redis dependencie

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,3 @@
-import redis
-
 from decouple import config
 
 
@@ -8,8 +6,3 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 SUPPORTED_LANGUAGES = config(
     'SUPPORTED_LANGUAGES',
     cast=lambda v: v.split())
-
-REDIS_CONNECTION = redis.ConnectionPool(
-    host=config('REDIS'),
-    port=config('REDIS_PORT'),
-    db=config('REDIS_DB'))
