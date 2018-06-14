ebb77732725e5c6bb259b9b9c2ed0f83bc81825c
Douglas Paz <eu@douglaspaz.com>
2018-06-14 13:48:54 -0300

    pep8

--- bothub-nlp/cli/import_supported_languages.py
+++ bothub-nlp/cli/import_supported_languages.py
@@ -1,10 +1,10 @@
 import plac
 
-from app.settings import SUPPORTED_LANGUAGES
-from . import import_langs
-
 
 @plac.annotations(
     extra_models=('add extra models dir', 'option', 'e'),)
 def import_supported_languages(extra_models=None):
+    from app.settings import SUPPORTED_LANGUAGES
+    from . import import_langs
+
     import_langs(extra_models, *SUPPORTED_LANGUAGES)
