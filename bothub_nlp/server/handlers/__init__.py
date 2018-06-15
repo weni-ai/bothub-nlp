cdc8d56ff5eaf289dbf9165ae81f15ed767116d4
Douglas Paz <eu@douglaspaz.com>
2018-06-15 11:38:05 -0300

    Add support to json body in ApiHandler

--- bothub_nlp/server/handlers/__init__.py
+++ bothub_nlp/server/handlers/__init__.py
@@ -1,15 +1,34 @@
+import tornado.escape
+
 from tornado.web import RequestHandler
+from rest_framework import status
 from bothub.common.models import RepositoryAuthorization
 
 
 class ApiHandler(RequestHandler):
+    content_type = 'application/json'
+
     def set_default_headers(self):
-        self.set_header('Content-Type', 'application/json')
+        self.set_header('Content-Type', self.content_type)
         self.set_header('Access-Control-Allow-Origin', '*')
         self.set_header(
             'Access-Control-Allow-Headers',
             'Content-Type, Authorization')
         self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    def prepare(self):
+        super().prepare()
+        self.json_data = None
+        request_content_type = self.request.headers.get('Content-Type', None)
+        if self.request.method in ['POST'] and \
+                self.request.body and \
+                request_content_type == 'application/json':
+            self.json_data = tornado.escape.json_decode(self.request.body)
+
+    def get_argument(self, arg, default=None):
+        if self.json_data:
+            return self.json_data.get(arg, default)
+        return super().get_argument(arg, default)
 
     def repository_authorization(self):
         authorization_header_value = self.request.headers.get('Authorization')
@@ -25,10 +44,14 @@ class ApiHandler(RequestHandler):
         except RepositoryAuthorization.DoesNotExist:
             return False
 
-    def options(self):
-        self.set_status(204)
+    def get(self):
+        self.set_status(status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.finish()
+
+    def post(self):
+        self.set_status(status.HTTP_405_METHOD_NOT_ALLOWED)
         self.finish()
 
     def options(self):
-        self.set_status(204)
+        self.set_status(status.HTTP_405_METHOD_NOT_ALLOWED)
         self.finish()
