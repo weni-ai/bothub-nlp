9ba7140a2417be5d5cceef6b6e046ce425309090
Douglas Paz <eu@douglaspaz.com>
2018-06-19 15:07:16 -0300

    Fix cli commands

--- bothub_nlp/cli/import_langs.py
+++ bothub_nlp/cli/import_langs.py
@@ -4,7 +4,7 @@ import plac
 @plac.annotations(
     extra_models=('add extra models dir', 'option', 'e'),)
 def import_langs(extra_models=None, *langs):
-    from . import import_lang
+    from .import_lang import import_lang
 
     print('Languages to import: {}'.format(', '.join(langs)))
     for lang in langs:
