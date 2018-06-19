83ccade8604eb240b325911cad075e67da5580c4
Douglas Paz <eu@douglaspaz.com>
2018-06-19 15:41:07 -0300

    Enable tornado logger

--- bothub_nlp/server/__init__.py
+++ bothub_nlp/server/__init__.py
@@ -10,10 +10,6 @@ logging.basicConfig(format=settings.LOGGER_FORMAT)
 logger = logging.getLogger('bothub NLP server')
 logger.setLevel(settings.LOGGER_LEVEL)
 
-logging.getLogger('tornado').disabled = True
-logging.getLogger('tornado.access').disabled = True
-logging.getLogger('tornado.application').disabled = True
-
 
 def make_app():
     return Application([
