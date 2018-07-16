0954458ebcccf7e5b18475e9afe46bc180987611
Douglas Paz <eu@douglaspaz.com>
2018-07-16 10:08:22 -0300

    Move import_ilha_spacy_langs to dockerfile

--- docker/bothub-nlp/entrypoint.sh
+++ docker/bothub-nlp/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 if [[ ${DOWNLOADED_LANGUAGES} != ${SUPPORTED_LANGUAGES} ]]; then make download_supported_languages; fi
-make import_ilha_spacy_langs CHECK_ENVIRONMENT=false
 make start CHECK_ENVIRONMENT=false
