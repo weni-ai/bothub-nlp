d91fb39f25ec7a0ec5f89110cf2e6d1f97bbe7f6
Douglas Paz <eu@douglaspaz.com>
2018-08-09 08:13:43 -0300

    Init custom train with labels

--- bothub_nlp/core/train.py
+++ bothub_nlp/core/train.py
@@ -3,9 +3,20 @@ from tempfile import mkdtemp
 from rasa_nlu.model import Trainer
 from rasa_nlu.training_data import Message, TrainingData
 from rasa_nlu.components import ComponentBuilder
+from django.db import models
 
 from .utils import get_rasa_nlu_config_from_update
 from .persistor import BothubPersistor
+
+
+def BothubTrainingData(object):
+    def __init__(self, label_training_examples=None, **kwargs):
+        if label_training_examples:
+            self.label_training_examples = self.sanitize_examples(
+                label_training_examples)
+        else:
+            self.label_training_examples = []
+        # super().__init__(**kwargs)
 
 
 def train_update(update, by):
@@ -13,18 +24,33 @@ def train_update(update, by):
 
     examples = [
         Message.build(
-            example.get_text(update.language),
-            example.intent,
-            [
+            text=example.get_text(update.language),
+            intent=example.intent,
+            entities=[
                 entity.rasa_nlu_data
                 for entity in example.get_entities(update.language)])
         for example in update.examples]
+
+    label_examples_query = update.examples \
+        .filter(entities__entity__label__isnull=False) \
+        .annotate(entities_count=models.Count('entities')) \
+        .filter(entities_count__gt=0)
+
+    label_examples = [
+        Message.build(
+            text=example.get_text(update.language),
+            entities=[
+                entity.get_rasa_nlu_data(label_as_entity=True)
+                for entity in example.get_entities(update.language)])
+        for example in label_examples_query]
 
     rasa_nlu_config = get_rasa_nlu_config_from_update(update)
     trainer = Trainer(
         rasa_nlu_config,
         ComponentBuilder(use_cache=False))
-    training_data = TrainingData(training_examples=examples)
+    training_data = BothubTrainingData(
+        label_training_examples=label_examples,
+        training_examples=examples)
     trainer.train(training_data)
     persistor = BothubPersistor(update)
     trainer.persist(
--- bothub_nlp/tests/utils.py
+++ bothub_nlp/tests/utils.py
@@ -67,6 +67,130 @@ EXAMPLES_MOCKUP = [
 ]
 
 
+EXAMPLES_WITH_LABEL_MOCKUP = [
+    {
+        'text': 'I love my dog, my puppy is awesome',
+        'intent': '',
+        'entities': [
+            {
+                'start': 10,
+                'end': 13,
+                'entity': 'dog',
+                'label': 'animal',
+            },
+            {
+                'start': 27,
+                'end': 34,
+                'entity': 'dog',
+                'label': 'animal',
+            },
+            {
+                'start': 0,
+                'end': 1,
+                'entity': 'i',
+                'label': 'person',
+            }
+        ],
+    },
+    {
+        'text': 'We love dogs!',
+        'intent': '',
+        'entities': [
+            {
+                'start': 8,
+                'end': 12,
+                'entity': 'dog',
+                'label': 'animal',
+            },
+            {
+                'start': 0,
+                'end': 2,
+                'entity': 'we',
+                'label': 'person',
+            }
+        ],
+    },
+    {
+        'text': 'We have a cat, cats are beautiful',
+        'intent': '',
+        'entities': [
+            {
+                'start': 10,
+                'end': 13,
+                'entity': 'cat',
+                'label': 'animal',
+            },
+            {
+                'start': 15,
+                'end': 19,
+                'entity': 'cat',
+                'label': 'animal',
+            },
+            {
+                'start': 0,
+                'end': 2,
+                'entity': 'we',
+                'label': 'person',
+            }
+        ],
+    },
+    {
+        'text': 'I love my cat',
+        'intent': '',
+        'entities': [
+            {
+                'start': 10,
+                'end': 13,
+                'entity': 'cat',
+                'label': 'animal',
+            },
+            {
+                'start': 0,
+                'end': 1,
+                'entity': 'i',
+                'label': 'person',
+            }
+        ]
+    },
+    {
+        'text': 'I love my aunt',
+        'intent': '',
+        'entities': [
+            {
+                'start': 10,
+                'end': 14,
+                'entity': 'aunt',
+                'label': 'person',
+            },
+            {
+                'start': 0,
+                'end': 1,
+                'entity': 'i',
+                'label': 'person',
+            }
+        ],
+    },
+    {
+        'text': 'My aunt love cats',
+        'intent': '',
+        'entities': [
+            {
+                'start': 3,
+                'end': 7,
+                'entity': 'aunt',
+                'label': 'person',
+            },
+            {
+                'start': 13,
+                'end': 17,
+                'entity': 'cat',
+                'label': 'animal',
+            }
+        ],
+    },
+]
+
+
 def fill_examples(examples_mockup, repository, language=None):
     for example_mockup in examples_mockup:
         example = RepositoryExample.objects.create(
@@ -75,8 +199,12 @@ def fill_examples(examples_mockup, repository, language=None):
             text=example_mockup.get('text'),
             intent=example_mockup.get('intent'))
         for entity_mockup in example_mockup.get('entities', []):
-            RepositoryExampleEntity.objects.create(
+            example_entity = RepositoryExampleEntity.objects.create(
                 repository_example=example,
                 start=entity_mockup.get('start'),
                 end=entity_mockup.get('end'),
                 entity=entity_mockup.get('entity'))
+            entity_label = entity_mockup.get('label')
+            if entity_label:
+                example_entity.entity.set_label(entity_label)
+                example_entity.entity.save()
