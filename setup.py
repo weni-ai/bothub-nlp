e6c3a9d4353e38a63b301998827b28d557bed1be
Douglas Paz <eu@douglaspaz.com>
2018-11-30 14:26:27 -0300

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub-nlp',
-    version='1.0.0',
+    version='1.1.0',
     description='Bothub NLP service',
     packages=find_packages(),
     install_requires=[
-        'bothub-engine==1.16.0',
+        'bothub-engine==1.17.0',
         'tornado==5.1.1',
         'contextvars==2.3',
         'raven==6.9.0',
-        'spacy==2.0.12',
+        'spacy==2.0.17',
         'rasa-nlu==0.13.1',
-        'tensorflow==1.11.0',
-        'scikit-learn==0.20.0',
+        'tensorflow==1.12.0',
+        'scikit-learn==0.20.1',
         'sklearn-crfsuite==0.3.6',
         'plac==0.9.6',
+        'msgpack==0.5.6',
     ],
     python_requires='>=3.6',
 )
