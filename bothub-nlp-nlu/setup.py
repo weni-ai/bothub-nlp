05d7cb5f3bb4dc39563ca5e5b64ca3f1aecaa84f
Douglas Paz <eu@douglaspaz.com>
2019-01-24 08:46:19 -0300

    Bump version bothub-nlp and bothub-nlp-nlu

--- bothub-nlp-nlu/setup.py
+++ bothub-nlp-nlu/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub-nlp-nlu',
-    version='0.1.0',
+    version='0.2.0',
     description='Bothub NLP NLU',
     packages=find_packages(),
     dependency_links=[
--- bothub-nlp/setup.py
+++ bothub-nlp/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub-nlp',
-    version='0.1.0',
+    version='0.2.0',
     description='Bothub NLP Common',
     packages=find_packages(),
     dependency_links=[
