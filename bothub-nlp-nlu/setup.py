12bc4115b802fe0bb08d93f30e4b1122c4e1787a
Douglas Paz <eu@douglaspaz.com>
2019-01-24 09:05:20 -0300

    Upgrade bothub-engine

--- bothub-nlp-nlu/setup.py
+++ bothub-nlp-nlu/setup.py
@@ -7,7 +7,7 @@ setup(
     description='Bothub NLP NLU',
     packages=find_packages(),
     dependency_links=[
-        'git+https://github.com/Ilhasoft/bothub-engine@1.19.0b#egg=bothub-en' +
+        'git+https://github.com/Ilhasoft/bothub-engine@1.19.0#egg=bothub-en' +
         'gine',
     ],
     install_requires=[
