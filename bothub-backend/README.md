202f1249569b15405d0a20a185ddadfc24af225a
Daniel Yohan <dyohan9@gmail.com>
2019-09-10 17:21:29 -0300

    Refact all packages

--- bothub-backend/README.md
+++
@@ -1,3 +0,0 @@
-# Bothub NLP Backend Package
-
-Usually this package is used as dependencie in a annother packages.
--- bothub-backend/bothub_backend/__init__.py
+++
@@ -1,9 +0,0 @@
-from pydoc import locate
-
-
-def get_backend(path, backend):
-    """
-    Path Example: bothub_backend.bothub.BothubBackend
-    Backend Example: https://api.bothub.it
-    """
-    return locate(path)(backend=backend)
