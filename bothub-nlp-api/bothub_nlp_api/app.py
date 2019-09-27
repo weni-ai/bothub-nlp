fb96062029b52b446fc69574992263cb486450c6
Daniel Yohan <dyohan9@gmail.com>
2019-09-27 16:57:53 -0300

    Removed Options

--- bothub-nlp-api/bothub_nlp_api/app.py
+++ bothub-nlp-api/bothub_nlp_api/app.py
@@ -28,7 +28,7 @@ def handle_invalid_usage(error):
     return response
 
 
-@app.route('/', methods=['GET', 'OPTIONS'])
+@app.route('/')
 @authorization_required
 def parse_handler():
     text = request.args.get('text', default=None)
@@ -41,7 +41,8 @@ def parse_handler():
     return parse._parse(text, language, rasa_format)
 
 
-@app.route('/parse/', methods=['POST', 'OPTIONS'])
+@app.route('/parse/', methods=['POST'])
+@crossdomain(origin)
 @authorization_required
 def parsepost_handler():
     jsondata = json.loads(request.data)
@@ -55,13 +56,13 @@ def parsepost_handler():
     return parse._parse(text, language, rasa_format)
 
 
-@app.route('/train/', methods=['POST', 'OPTIONS'])
+@app.route('/train/', methods=['POST'])
 @authorization_required
 def train_handler():
     return train.train_handler()
 
 
-@app.route('/info/', methods=['GET', 'OPTIONS'])
+@app.route('/info/')
 @authorization_required
 def info_handler():
     repository_authorization = get_repository_authorization()
@@ -71,7 +72,7 @@ def info_handler():
     return resp
 
 
-@app.route('/evaluate/', methods=['POST', 'OPTIONS'])
+@app.route('/evaluate/', methods=['POST'])
 @authorization_required
 def evaluate_handler():
     return evaluate.evaluate_handler()
