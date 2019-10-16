2c2466b546519920f667cf8d0697731e6c6c87bb
Daniel Yohan <dyohan9@gmail.com>
2019-10-16 13:16:13 -0300

    Revert "Test Running with Flask"

--- bothub-nlp-api/bothub_nlp_api/wsgi.py
+++ bothub-nlp-api/bothub_nlp_api/wsgi.py
@@ -1,4 +1,4 @@
-from .app import app as application
+from bothub_nlp_api.app import app as application
 
 if __name__ == "__main__":
     application.run()
--- bothub-nlp-api/entrypoint.sh
+++ bothub-nlp-api/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cd $WORKDIR
 
-#gunicorn bothub_nlp_api.wsgi --log-level debug --timeout 999999 -c gunicorn.conf.py
-python app.py
+gunicorn bothub_nlp_api.wsgi --log-level debug --timeout 999999 -c gunicorn.conf.py
