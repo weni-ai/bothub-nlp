69b7a6a5bb5d1d925b10e58b9510f4884f9d7be8
Daniel Yohan <dyohan9@gmail.com>
2019-11-06 13:53:57 -0300

    Updated Gunicorn

--- bothub-nlp-api/entrypoint.sh
+++ bothub-nlp-api/entrypoint.sh
@@ -2,4 +2,4 @@
 cd $WORKDIR
 
 #gunicorn bothub_nlp_api.wsgi --log-level debug --timeout 999999 -c gunicorn.conf.py
-gunicorn -k uvicorn.workers.UvicornWorker -c "gunicorn.conf.py" "bothub_nlp_api.app:app"
+gunicorn -k uvicorn.workers.UvicornWorker --timeout 999999 -c "gunicorn.conf.py" "bothub_nlp_api.app:app"
