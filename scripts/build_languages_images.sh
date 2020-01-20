9a4283a3442ad378cf0b8b7dda60f8d02f851797
Daniel Yohan <dyohan9@gmail.com>
2020-01-20 11:08:46 -0300

    [fix] Build Languages Images

--- scripts/build_languages_images.sh
+++ scripts/build_languages_images.sh
@@ -6,7 +6,7 @@ for language in $LANGUAGES_LIST
 do
     language_split=$(echo $language | tr ":" " ")
     docker_tag=$(echo $language_split | head -n1 | awk '{print $1;}')
-    BOTHUB_NLP_NLU_WORKER_DOCKER_IMAGE_TAG=$docker_tag docker-compose build --build-arg DOWNLOAD_SPACY_MODELS=${language} bothub-nlp-nlu-worker
+    BOTHUB_NLP_NLU_WORKER_DOCKER_IMAGE_TAG=$docker_tag docker-compose build --build-arg DOWNLOAD_SPACY_MODELS=${language}
 done
 
 if [ ${PUSH_IMAGE} = true ];
