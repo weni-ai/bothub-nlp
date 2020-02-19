94648d83677470794ae849f0b86f3041ce7e80c2
lucasraggi89 <lrr@ic.ufal.br>
2020-02-19 10:54:12 -0300

    most_similar only getting lowercase words to avoid duplicates

--- bothub_nlp_nlu_worker/bothub_nlp_nlu/sentence_suggestion.py
+++ bothub_nlp_nlu_worker/bothub_nlp_nlu/sentence_suggestion.py
@@ -2,13 +2,13 @@ from collections import OrderedDict
 from thinc.neural.util import get_array_module
 from bothub_nlp_celery.app import nlp_language
 import random
-import time
 
 
 class SentenceSuggestion:
     def __init__(self):
         self.nlp = nlp_language
         self.to_replace_tags = ["VERB", "NOUN", "ADJ", "ADV", "INTJ", "PROPN"]
+        self.n_highest = 50
         self.row2key = {row: key for key, row in self.nlp.vocab.vectors.key2row.items()}
 
     def get_words_to_replace_idx(self, similar_words_json, word_list, percentage_to_replace):
@@ -43,7 +43,7 @@ class SentenceSuggestion:
                     "similar_words": [],
                 }
                 if nlp_sentence[i].pos_ in self.to_replace_tags:
-                    similar_words = self.most_similar(nlp_sentence[i].text, n=6)[0]
+                    similar_words = self.most_similar(nlp_sentence[i].text, topn=6)
                     similar_words_size = len(similar_words)
                     for j in range(similar_words_size):
                         nlp_similar = self.nlp(similar_words[j][0])
@@ -89,45 +89,45 @@ class SentenceSuggestion:
     def idx_to_word(self, idx):
         return self.nlp.vocab[self.row2key[idx]].text
 
-    def most_similar(self, words, *, batch_size=1024, n=1, sort=True):
-        start = time.time()
+    def most_similar(self, input_word, *, batch_size=1024, topn=1, sort=True):
         xp = get_array_module(self.nlp.vocab.vectors.data)
-        queries = self.words_to_vecs(xp, words)
-        best_rows = xp.zeros((queries.shape[0], n), dtype='i')
-        scores = xp.zeros((queries.shape[0], n), dtype='f')
+        input_vector = self.nlp(input_word).vector.reshape(1, self.nlp.vocab.vectors.shape[1])
+        best_rows = xp.zeros((1, self.n_highest), dtype='i')
+        scores = xp.zeros((1, self.n_highest), dtype='f')
         # Work in batches, to avoid memory problems.
-        print('setup in: ', time.time() - start)
-        start_iter = time.time()
-        for i in range(0, queries.shape[0], batch_size):
-            batch = queries[i: i + batch_size]
+        for i in range(0, input_vector.shape[0], batch_size):
+            batch = input_vector[i: i + batch_size]
             batch_norms = xp.linalg.norm(batch, axis=1, keepdims=True)
             batch_norms[batch_norms == 0] = 1
             batch /= batch_norms
             sims = xp.dot(batch, self.nlp.vocab.vectors.data.T)
-            best_rows[i:i + batch_size] = xp.argpartition(sims, -n, axis=1)[:, -n:]
-            scores[i:i + batch_size] = xp.partition(sims, -n, axis=1)[:, -n:]
+            best_rows[i:i + batch_size] = xp.argpartition(sims, -self.n_highest, axis=1)[:, -self.n_highest:]
+            scores[i:i + batch_size] = xp.partition(sims, -self.n_highest, axis=1)[:, -self.n_highest:]
 
-        if sort and n >= 2:
+        if sort and topn >= 2:
             sorted_index = xp.arange(scores.shape[0])[:, None][i:i + batch_size], xp.argsort(scores[i:i + batch_size],
                                                                                              axis=1)[:, ::-1]
             scores[i:i + batch_size] = scores[sorted_index]
             best_rows[i:i + batch_size] = best_rows[sorted_index]
-        print('iter in: ', time.time() - start_iter)
-        start_end = time.time()
+
         scores = xp.around(scores, decimals=4, out=scores)
         scores = xp.clip(scores, a_min=-1, a_max=1, out=scores)
-        scores_list = scores.tolist()
-        best_rows = xp.vectorize(self.idx_to_word)(best_rows)
-        rows_list = best_rows.tolist()
-        similar_tuple_list = []
-        size = len(scores_list)
-        for i in range(size):
-            similar_tuple_list.append(list(zip(rows_list[i], scores_list[i])))
 
-        for tuple_list in similar_tuple_list:
-            print(tuple_list)
-        print('wrapping up in: ', time.time() - start_end)
-        return similar_tuple_list
+        similar_list = []
+        for i in range(self.n_highest):
+            row = best_rows[0][i]
+            score = scores[0][i]
+            candidate_word_vocab = self.nlp.vocab[self.row2key[row]]
+            candidate_word = candidate_word_vocab.text
+            if (
+                candidate_word_vocab.is_lower == input_word.islower()
+                and candidate_word != input_word
+            ):
+                similar_list.append((candidate_word, score))
+            if len(similar_list) >= topn:
+                break
+
+        return similar_list
 
 
 def sentence_suggestion_text(text):
