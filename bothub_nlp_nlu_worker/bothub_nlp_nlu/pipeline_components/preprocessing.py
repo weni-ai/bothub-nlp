fab8438b2ad68a7bc286958844ce4e1671e58831
França Mac Dowell <franca.sales@ilhasoft.com.br>
2020-05-15 14:24:34 -0300

    Add new steps to preprocessing to english and portuguese words

--- bothub_nlp_nlu_worker/bothub_nlp_nlu/pipeline_components/preprocessing.py
+++ bothub_nlp_nlu_worker/bothub_nlp_nlu/pipeline_components/preprocessing.py
@@ -26,6 +26,107 @@ class Preprocessing(Component):
     def __init__(self, component_config: Optional[Dict[Text, Any]] = None) -> None:
         super().__init__(component_config)
 
+    def portuguese_preprocesing(self, phrase):
+
+        phrase = phrase.replace("blza", "beleza")
+        phrase = phrase.replace("flw", "falou")
+        phrase = phrase.replace("vlw", "valeu")
+        phrase = phrase.replace("ok", "okay")
+        phrase = phrase.replace("tranks", "tranquilo")
+        phrase = phrase.replace("mkt", "marketing")
+
+        # set regex to "blz"
+        blz_regex = r"\b(bl)z*\b"
+        # set regex for "n":
+        n_regex = r"\b(n|N)\1*\b"
+        # set regex for "s":
+        s_regex = r"\b(s|S)\1*\b"
+        # set regex for "to":
+        to_regex = r"\b(t)o*\b"
+        # set regex for "ta":
+        ta_regex = r"\b(t)o*\b"
+        # set regex for "mkt":
+        mkt_regex = r"\b(mkt)\b"
+        # set regex for "ok":
+        ok_regex = r"\b(ok)\b"
+        # set regex for "bd":
+        bd_regex = r"\b(bd)\b"
+
+        # set replace words
+        S_WORD = "sim"
+        N_WORD = "nao"
+        BLZ_WORD = "beleza"
+        TO_WORD = "estou"
+        TA_WORD = "esta"
+        MKT_WORD = "marketing"
+        OK_WORD = "okay"
+        BD_WORD = "bom dia"
+
+        # replace regex by S_WORD"
+        phrase = re.sub(s_regex, S_WORD, phrase)
+        # replace regex by N_WORD
+        phrase = re.sub(n_regex, N_WORD, phrase)
+        # replace regex by BLZ_WORD
+        phrase = re.sub(blz_regex, BLZ_WORD, phrase)
+        # replace regex by TO_WORD
+        phrase = re.sub(to_regex, TO_WORD, phrase)
+        # replace regex by TA_WORD
+        phrase = re.sub(ta_regex, TA_WORD, phrase)
+        # replace regex by MKT_WORD
+        phrase = re.sub(mkt_regex, MKT_WORD, phrase)
+        # replace regex by OK_WORD
+        phrase = re.sub(ok_regex, OK_WORD, phrase)
+        # replace regex by BD_WORD
+        phrase = re.sub(bd_regex, BD_WORD, phrase)
+
+        return phrase
+
+
+    def english_preprocesing(self, phrase):
+        # set regex for "mkt":
+        mkt_regex = r"\b(mkt)\b"
+        # set regex for "ok":
+        ok_regex = r"\b(ok)\b"
+        # set regex for "ty":
+        ty_regex = r"\b(ty)\b"
+        # set regex for "thx":
+        thx_regex = r"\b(thx)\b"
+        # set regex for "tks":
+        tks_regex = r"\b(tks)\b"
+        # set regex for " 'm / 'mmmm ":
+        am_regex = r"('m)m*\b"
+        # set regex for " 'm / 'mmmm ":
+        are_regex = r"('re)e*\b"
+        # set regex for " *n't ":
+        not_regex = r"(n't)\b"
+
+        # set replace words
+        MKT_WORD = "marketing"
+        OK_WORD = "okay"
+        TY_WORD = "thank you"
+        AM_WORD = " am"
+        ARE_WORD = " are"
+        NOT_WORD = " not"
+
+        # replace regex by MKT_WORD
+        phrase = re.sub(mkt_regex, MKT_WORD, phrase)
+        # replace regex by OK_WORD
+        phrase = re.sub(ok_regex, OK_WORD, phrase)
+        # replace regex by TY_WORD
+        phrase = re.sub(ty_regex, TY_WORD, phrase)
+        # replace regex by THX_WORD
+        phrase = re.sub(thx_regex, TY_WORD, phrase)
+        # replace regex by TKS_WORD
+        phrase = re.sub(tks_regex, TY_WORD, phrase)
+        # replace regex by AM_WORD
+        phrase = re.sub(am_regex, AM_WORD, phrase)
+        # replace regex by ARE_WORD
+        phrase = re.sub(are_regex, ARE_WORD, phrase)
+        # replace regex by NOT_WORD
+        phrase = re.sub(not_regex, NOT_WORD, phrase)
+
+        return phrase
+
     def train(
         self,
         training_data: TrainingData,
@@ -38,27 +139,16 @@ class Preprocessing(Component):
         size = len(training_data.training_examples)
         subtract_idx = 0
 
-        APOSTROPHE_OPTIONS = ["'", "`"]
-
         for idx in range(size):
             example_text = training_data.training_examples[idx - subtract_idx].text
             # removing accent and lowercasing characters
             example_text = unidecode(example_text.lower())
-            # remove apostrophe from the phrase (important be first than s_regex regex)
-            for APOSTROPHE in APOSTROPHE_OPTIONS:
-                example_text = example_text.replace(APOSTROPHE, "")
+
+            if config.language == "en":
+                example_text = self.english_preprocesing(example_text)
 
             if config.language == "pt_br":
-                # set regex parameters
-                n_regex = r"\b(n|N)\1*\b"
-                s_regex = r"\b(s|S)\1*\b"
-                # set replace words
-                S_WORD = "sim"
-                N_WORD = "nao"
-                # replace regex by "sim"
-                example_text = re.sub(s_regex, S_WORD, example_text)
-                # replace regex by "nao"
-                example_text = re.sub(n_regex, N_WORD, example_text)
+                example_text = self.portuguese_preprocesing(example_text)
 
             if example_text in not_repeated_phrases:
                 # remove example at this index from training_examples
@@ -66,6 +156,7 @@ class Preprocessing(Component):
                 subtract_idx += 1
             else:
                 not_repeated_phrases.add(example_text)
+                print(example_text)
                 training_data.training_examples[idx - subtract_idx].text = example_text
 
     def process(
@@ -83,15 +174,10 @@ class Preprocessing(Component):
         # remove apostrophe from the phrase (important be first than s_regex regex)
         for APOSTROPHE in APOSTROPHE_OPTIONS:
             message.text = message.text.replace(APOSTROPHE, "")
-        if config.language == "pt_br":
-            # set regex parameters
-            n_regex = r"\b(n|N)\1*\b"
-            s_regex = r"\b(s|S)\1*\b"
-            # set replace words
-            S_WORD = "sim"
-            N_WORD = "nao"
 
-            # replace regex by "sim"
-            message.text = re.sub(s_regex, S_WORD, message.text)
-            # replace regex by "nao"
-            message.text = re.sub(n_regex, N_WORD, message.text)
+        if config.language == "en":
+            message.text = self.english_preprocesing(message.text)
+            print(message.text)
+
+        if config.language == "pt_br":
+            message.text = self.portuguese_preprocesing(message.text)
