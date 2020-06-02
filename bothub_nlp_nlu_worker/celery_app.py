112913294a7fa248abb56fd3e48a0c16d9ae10b1
Daniel Yohan <dyohan9@gmail.com>
2020-06-02 16:24:36 -0300

    Update train

--- bothub_nlp_nlu_worker/celery_app.py
+++ bothub_nlp_nlu_worker/celery_app.py
@@ -1,3 +1,5 @@
+from bothub_nlp_celery import settings
+from bothub_nlp_celery.actions import send_job_ai_plataform
 from bothub_nlp_celery.app import celery_app
 from bothub_nlp_celery.tasks import TASK_NLU_PARSE_TEXT
 from bothub_nlp_celery.tasks import TASK_NLU_DEBUG_PARSE_TEXT
@@ -5,12 +7,11 @@ from bothub_nlp_celery.tasks import TASK_NLU_SENTENCE_SUGGESTION_TEXT
 from bothub_nlp_celery.tasks import TASK_NLU_TRAIN_UPDATE
 from bothub_nlp_celery.tasks import TASK_NLU_EVALUATE_UPDATE
 from bothub_nlp_celery.tasks import TASK_NLU_WORDS_DISTRIBUTION
-from bothub_nlp_rasa_utils import parse
+from bothub_nlp_rasa_utils import parse, train
 from bothub_nlp_rasa_utils import debug_parse
 from bothub_nlp_rasa_utils import sentence_suggestion
 from bothub_nlp_rasa_utils import evaluate
 from bothub_nlp_rasa_utils import words_distribution
-from bothub_nlp_nlu.train import train_update as train_update_core
 
 
 @celery_app.task(name=TASK_NLU_PARSE_TEXT)
@@ -30,7 +31,9 @@ def sentence_suggestion_text(*args, **kwargs):
 
 @celery_app.task(name=TASK_NLU_TRAIN_UPDATE)
 def train_update(repository_version, by_id, repository_authorization):
-    return train_update_core(repository_version, by_id, repository_authorization)
+    if settings.BOTHUB_NLP_AI_PLATFORM:
+        return send_job_ai_plataform(repository_version, by_id, repository_authorization)
+    return train.train_update(repository_version, by_id, repository_authorization)
 
 
 @celery_app.task(name=TASK_NLU_EVALUATE_UPDATE)
