0602c246199b535e25eb49561dde75068a88c5f8
Lucas Raggi <lrr@ic.ufal.br>
2020-06-02 15:13:41 -0300

    add aiplataform wireframe on train

--- bothub_nlp_nlu_worker/bothub_nlp_nlu/train.py
+++ bothub_nlp_nlu_worker/bothub_nlp_nlu/train.py
@@ -16,6 +16,8 @@ from .utils import get_examples_request
 from .persistor import BothubPersistor
 from . import logger
 
+from bothub_nlp_rasa_utils import train
+
 
 class BothubWriter(TrainingDataWriter):
     def dumps(self, training_data, **kwargs):  # pragma: no cover
@@ -45,49 +47,46 @@ class BothubWriter(TrainingDataWriter):
         )
 
 
+def send_job_ai_plataform(repository_version, by, repository_authorization):
+    training_inputs = {
+        'scaleTier': 'BASIC',
+        'masterConfig': {
+            "imageUri": 'gcr.io/bothub-273521/quickstart-image@sha256:f532b87cbb1c5db47586cff35a4afdfc3fefc23c3b8ad3174ef7688b3c503e8e',
+        },
+        'packageUris': [
+            'gs://poc-training-ai-platform/job-dir/packages/82add65c49cb4a75897aec4b3832299824e68d494a47be7599d4904bace93912/ai-platform-poc-0.1.tar.gz'],
+        'pythonModule': 'trainer.task',
+        'args': ['--dataset', 'iac2.md', '--config', 'diet-pipeline2.yml'],
+        'region': 'us-east1',
+        'jobDir': 'gs://poc-training-ai-platform/job-dir',
+    }
+
+    job_spec = {'jobId': 'my_training_job_3', 'trainingInput': training_inputs}
+
+    # Salve o ID do projeto no formato necessário para as APIs, "projects/projectname":
+    project_name = 'bothub-273521'
+    project_id = 'projects/{}'.format(project_name)
+
+    # Consiga uma representação em Python dos serviços do AI Platform Training:
+    cloudml = discovery.build('ml', 'v1')
+
+    # Crie e envie sua solicitação:
+    request = cloudml.projects().jobs().create(body=job_spec,
+                                               parent=project_id)
+
+    try:
+        response = request.execute()
+        print("SUCCESS!!!")
+        return response
+
+    except errors.HttpError as err:
+        logging.error('/n There was an error creating the training job.'
+                      ' Check the details:')
+        logging.error(err._get_reason())
+
+
 def train_update(repository_version, by, repository_authorization):  # pragma: no cover
-    update_request = backend().request_backend_start_training_nlu(
-        repository_version, by, repository_authorization
-    )
-
-    examples_list = get_examples_request(repository_version, repository_authorization)
-
-    with PokeLogging() as pl:
-        try:
-            examples = []
-
-            for example in examples_list:
-                examples.append(
-                    Message.build(
-                        text=example.get("text"),
-                        intent=example.get("intent"),
-                        entities=example.get("entities"),
-                    )
-                )
-
-            rasa_nlu_config = get_rasa_nlu_config_from_update(update_request)
-            trainer = Trainer(rasa_nlu_config, ComponentBuilder(use_cache=False))
-            training_data = TrainingData(training_examples=examples)
-
-            trainer.train(training_data)
-
-            persistor = BothubPersistor(
-                repository_version, repository_authorization, rasa_version
-            )
-            trainer.persist(
-                mkdtemp(),
-                persistor=persistor,
-                fixed_model_name=f"{update_request.get('repository_version')}_"
-                f"{update_request.get('total_training_end')+1}_"
-                f"{update_request.get('language')}",
-            )
-        except Exception as e:
-            logger.exception(e)
-            backend().request_backend_trainfail_nlu(
-                repository_version, repository_authorization
-            )
-            raise e
-        finally:
-            backend().request_backend_traininglog_nlu(
-                repository_version, pl.getvalue(), repository_authorization
-            )
+    if settings.GOOGLE:
+        send_job_ai_plataform(repository_version, by, repository_authorization)
+    else:
+        train(repository_version, by, repository_authorization)
