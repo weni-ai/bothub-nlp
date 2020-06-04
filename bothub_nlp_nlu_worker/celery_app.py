33cbf49591d4216f11af66fb315a080d226d317f
Lucas Raggi <lrr@ic.ufal.br>
2020-06-04 19:01:24 -0300

    train, parse, debug_parse working with bothub_nlp_rasa_utils

--- bothub_nlp_nlu_worker/celery_app.py
+++ bothub_nlp_nlu_worker/celery_app.py
@@ -1,5 +1,4 @@
 from bothub_nlp_celery import settings
-from bothub_nlp_celery.actions import send_job_ai_plataform
 from bothub_nlp_celery.app import celery_app
 from bothub_nlp_celery.tasks import TASK_NLU_PARSE_TEXT
 from bothub_nlp_celery.tasks import TASK_NLU_DEBUG_PARSE_TEXT
@@ -7,26 +6,36 @@ from bothub_nlp_celery.tasks import TASK_NLU_SENTENCE_SUGGESTION_TEXT
 from bothub_nlp_celery.tasks import TASK_NLU_TRAIN_UPDATE
 from bothub_nlp_celery.tasks import TASK_NLU_EVALUATE_UPDATE
 from bothub_nlp_celery.tasks import TASK_NLU_WORDS_DISTRIBUTION
-from bothub_nlp_rasa_utils import parse, train
-from bothub_nlp_rasa_utils import debug_parse
-from bothub_nlp_rasa_utils import sentence_suggestion
-from bothub_nlp_rasa_utils import evaluate
-from bothub_nlp_rasa_utils import words_distribution
+from bothub_nlp_celery.actions import send_job_ai_plataform
+from bothub_nlp_nlu.parse import parse_text as parse_text_core
+from bothub_nlp_nlu.debug_parse import debug_parse_text as debug_parse_text_core
+from bothub_nlp_nlu.sentence_suggestion import (
+    sentence_suggestion_text as sentence_suggestion_text_core,
+)
+from bothub_nlp_nlu.words_distribution import (
+    words_distribution_text as words_distribution_core,
+)
+from bothub_nlp_rasa_utils import train
+from bothub_nlp_nlu.evaluate import evaluate_update as evaluate_update_core
 
 
 @celery_app.task(name=TASK_NLU_PARSE_TEXT)
 def parse_text(repository_version, repository_authorization, *args, **kwargs):
-    return parse.parse_text(repository_version, repository_authorization, *args, **kwargs)
+    return parse_text_core(
+        repository_version, repository_authorization, *args, **kwargs
+    )
 
 
 @celery_app.task(name=TASK_NLU_DEBUG_PARSE_TEXT)
 def debug_parse_text(repository_version, repository_authorization, *args, **kwargs):
-    return debug_parse.debug_parse_text(repository_version, repository_authorization, *args, **kwargs)
+    return debug_parse_text_core(
+        repository_version, repository_authorization, *args, **kwargs
+    )
 
 
 @celery_app.task(name=TASK_NLU_SENTENCE_SUGGESTION_TEXT)
 def sentence_suggestion_text(*args, **kwargs):
-    return sentence_suggestion.sentence_suggestion_text(*args, **kwargs)
+    return sentence_suggestion_text_core(*args, **kwargs)
 
 
 @celery_app.task(name=TASK_NLU_TRAIN_UPDATE)
@@ -38,9 +47,11 @@ def train_update(repository_version, by_id, repository_authorization):
 
 @celery_app.task(name=TASK_NLU_EVALUATE_UPDATE)
 def evaluate_update(repository_version, by_id, repository_authorization):
-    return evaluate.evaluate_update(repository_version, repository_authorization)
+    return evaluate_update_core(repository_version, by_id, repository_authorization)
 
 
 @celery_app.task(name=TASK_NLU_WORDS_DISTRIBUTION)
-def words_distribution_update(repository_version, language, repository_authorization):
-    return words_distribution.words_distribution_text(repository_version, language, repository_authorization)
+def words_distribution(repository_version, language, repository_authorization):
+    return words_distribution_core(
+        repository_version, language, repository_authorization
+    )
