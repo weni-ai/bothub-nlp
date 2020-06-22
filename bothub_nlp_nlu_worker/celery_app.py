c5e4c7a7833546a653eefa126deba980f3b39b48
Daniel Yohan <dyohan9@gmail.com>
2020-06-22 20:52:54 -0300

    Update celery app

--- bothub_nlp_nlu_worker/celery_app.py
+++ bothub_nlp_nlu_worker/celery_app.py
@@ -15,6 +15,7 @@ from bothub_nlp_nlu.words_distribution import (
     words_distribution_text as words_distribution_core,
 )
 from bothub_nlp_rasa_utils import train, evaluate
+from bothub_nlp_rasa_utils.utils import backend
 
 
 @celery_app.task(name=TASK_NLU_PARSE_TEXT)
@@ -38,7 +39,13 @@ def sentence_suggestion_text(*args, **kwargs):
 
 @celery_app.task(name=TASK_NLU_TRAIN_UPDATE)
 def train_update(repository_version, by_id, repository_authorization):
-    return train.train_update(repository_version, by_id, repository_authorization, celery_app.current_task.request.id)
+    backend().request_backend_save_queue_id(
+        repository_version=repository_version,
+        repository_authorization=repository_authorization,
+        task_id=celery_app.current_task.request.id,
+        from_queue=1
+    )
+    return train.train_update(repository_version, by_id, repository_authorization)
 
 
 @celery_app.task(name=TASK_NLU_EVALUATE_UPDATE)
