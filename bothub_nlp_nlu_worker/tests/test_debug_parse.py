bcaf3a38ca2b014b81c7235c3d4030e030119ffd
Lucas Agra <lucasomena@hotmail.com>
2020-10-26 16:39:02 -0300

    updating tests

--- bothub_nlp_nlu_worker/tests/test_debug_parse.py
+++ bothub_nlp_nlu_worker/tests/test_debug_parse.py
@@ -5,48 +5,39 @@ import base64
 import os
 from unittest.mock import patch
 
-from celery_app import debug_parse_text
+from celery_app_test import debug_parse_text
 
 
 class TestDebugParseTask(unittest.TestCase):
-
     def setUp(self, *args):
         self.repository_authorization = uuid.uuid4()
         self.current_update = {
             "ready_for_train": True,
             "current_version_id": 6647,
-            "repository_authorization_user_id": 303
+            "repository_authorization_user_id": 303,
         }
-        # change directory to load /models
-        os.chdir("../bothub_nlp_nlu_worker")
+        # change directory to load /model
+        cur_dir = os.getcwd().split("/")[-1]
+        if cur_dir == "tests":
+            os.chdir("../")
 
-    @patch(
-        "bothub_backend.bothub.BothubBackend.request_backend_parse_nlu_persistor",
-        return_value={
-            "version_id": 6647,
-            "total_training_end": 4,
-            "language": "pt_br",
-            "repository_uuid": "e1e8a0fa-625c-4ba3-8b91-4c9f308db791"
-        },
-    )
     @patch(
         "bothub_backend.bothub.BothubBackend.request_backend_parse_nlu_persistor",
         return_value={
             "version_id": 49,
             "repository_uuid": "0f6b9644-db55-49a2-a20d-2af74106d892",
             "total_training_end": 3,
-            "language": "pt_br",
-            "bot_data": base64.b64encode(open('example_bert_pt_br.tar.gz', 'rb').read())},
+            "language": "en",
+            "bot_data": base64.b64encode(
+                open("example_generic_language.tar.gz", "rb").read()
+            ),
+        },
     )
     @patch(
         "bothub_backend.bothub.BothubBackend.request_backend_info",
-        return_value={
-            "intents": ["affirmative", "negative", "doubt", "bias"]
-        },
+        return_value={"intents": ["affirmative", "negative", "doubt", "bias"]},
     )
-
     def test_debug_parse_without_rasa_format(self, *args):
-
         result = debug_parse_text(
             self.current_update.get("current_version_id"),
             self.repository_authorization,
@@ -54,32 +45,21 @@ class TestDebugParseTask(unittest.TestCase):
         )
         print(json.dumps(result, indent=2))
 
-        self.assertEqual("affirmative", result.get("intent").get("name"))
-        self.assertEqual(4, len(result.get("words").get("ok")))
-
-    @patch(
-        "bothub_backend.bothub.BothubBackend.request_backend_parse_nlu_persistor",
-        return_value={
-            "version_id": 6647,
-            "total_training_end": 4,
-            "language": "pt_br",
-            "repository_uuid": "e1e8a0fa-625c-4ba3-8b91-4c9f308db791"
-        },
-    )
     @patch(
         "bothub_backend.bothub.BothubBackend.request_backend_parse_nlu_persistor",
         return_value={
             "version_id": 49,
             "repository_uuid": "0f6b9644-db55-49a2-a20d-2af74106d892",
             "total_training_end": 3,
-            "language": "pt_br",
-            "bot_data": base64.b64encode(open('example_bert_pt_br.tar.gz', 'rb').read())},
+            "language": "en",
+            "bot_data": base64.b64encode(
+                open("example_generic_language.tar.gz", "rb").read()
+            ),
+        },
     )
     @patch(
         "bothub_backend.bothub.BothubBackend.request_backend_info",
-        return_value={
-            "intents": ["affirmative", "negative", "doubt", "bias"]
-        },
+        return_value={"intents": ["affirmative", "negative", "doubt", "bias"]},
     )
     def test_debug_parse_with_rasa_format(self, *args):
 
@@ -90,6 +70,3 @@ class TestDebugParseTask(unittest.TestCase):
             True,
         )
         print(json.dumps(result, indent=2))
-
-        self.assertEqual("affirmative", result.get("intent").get("name"))
-        self.assertEqual(4, len(result.get("words").get("ok")))
--- bothub_nlp_nlu_worker/tests/test_evaluate.py
+++ bothub_nlp_nlu_worker/tests/test_evaluate.py
@@ -4,20 +4,21 @@ import base64
 import os
 from unittest.mock import patch
 
-from celery_app import evaluate_update
+from celery_app_test import evaluate_update
 
 
 class TestEvaluateTask(unittest.TestCase):
-
     def setUp(self, *args):
         self.repository_authorization = uuid.uuid4()
         self.current_update = {
             "ready_for_train": True,
             "current_version_id": 6647,
-            "repository_authorization_user_id": 303
+            "repository_authorization_user_id": 303,
         }
-        # change directory to load /models
-        os.chdir("../bothub_nlp_nlu_worker")
+        # change directory to load /model
+        cur_dir = os.getcwd().split("/")[-1]
+        if cur_dir == "tests":
+            os.chdir("../")
 
     @patch(
         "bothub_backend.bothub.BothubBackend.request_backend_start_evaluation",
@@ -31,15 +32,27 @@ class TestEvaluateTask(unittest.TestCase):
             {"text": "ja namorei", "intent": "affirmative", "entities": []},
             {"text": "já namorei", "intent": "affirmative", "entities": []},
             {"text": "simn", "intent": "affirmative", "entities": []},
-            {"text": "aceito sim, muito obrigado", "intent": "affirmative", "entities": []},
+            {
+                "text": "aceito sim, muito obrigado",
+                "intent": "affirmative",
+                "entities": [],
+            },
             {"text": "sim, quero o documento", "intent": "affirmative", "entities": []},
             {"text": "não posso fazer isso", "intent": "negative", "entities": []},
             {"text": "não gostei", "intent": "negative", "entities": []},
             {"text": "deixa para lá", "intent": "negative", "entities": []},
             {"text": "não inventa história", "intent": "negative", "entities": []},
-            {"text": "não queria ter que dizer isso", "intent": "negative", "entities": []},
+            {
+                "text": "não queria ter que dizer isso",
+                "intent": "negative",
+                "entities": [],
+            },
             {"text": "não gostei daquele dia", "intent": "negative", "entities": []},
-            {"text": "nem deve ser tão bom assim", "intent": "negative", "entities": []},
+            {
+                "text": "nem deve ser tão bom assim",
+                "intent": "negative",
+                "entities": [],
+            },
             {"text": "não aceito", "intent": "negative", "entities": []},
             {"text": "nop deixa de onda", "intent": "negative", "entities": []},
             {"text": "melhor nao falar nada", "intent": "negative", "entities": []},
@@ -66,16 +79,32 @@ class TestEvaluateTask(unittest.TestCase):
             {"text": "podemos marcar sim", "intent": "affirmative", "entities": []},
             {"text": "quero sim", "intent": "affirmative", "entities": []},
             {"text": "pode contar comigo", "intent": "affirmative", "entities": []},
-            {"text": "posso sim! me confirma a data", "intent": "affirmative", "entities": []},
-            {"text": "claro que estou disponivel", "intent": "affirmative", "entities": []},
+            {
+                "text": "posso sim! me confirma a data",
+                "intent": "affirmative",
+                "entities": [],
+            },
+            {
+                "text": "claro que estou disponivel",
+                "intent": "affirmative",
+                "entities": [],
+            },
             {"text": "ótima ideia", "intent": "affirmative", "entities": []},
-            {"text": "seria legal se fossemos", "intent": "affirmative", "entities": []},
+            {
+                "text": "seria legal se fossemos",
+                "intent": "affirmative",
+                "entities": [],
+            },
             {"text": "que legal, gosto sim", "intent": "affirmative", "entities": []},
             {"text": "é possivel", "intent": "affirmative", "entities": []},
             {"text": "pode me mandar sim", "intent": "affirmative", "entities": []},
             {"text": "aceito", "intent": "affirmative", "entities": []},
             {"text": "dá sim", "intent": "affirmative", "entities": []},
-            {"text": "adorei a ideia vamos sim", "intent": "affirmative", "entities": []},
+            {
+                "text": "adorei a ideia vamos sim",
+                "intent": "affirmative",
+                "entities": [],
+            },
             {"text": "quero", "intent": "affirmative", "entities": []},
             {"text": "vamos sim", "intent": "affirmative", "entities": []},
             {"text": "claro", "intent": "affirmative", "entities": []},
@@ -99,9 +128,17 @@ class TestEvaluateTask(unittest.TestCase):
             {"text": "não estou namorando", "intent": "negative", "entities": []},
             {"text": "a ta sei", "intent": "affirmative", "entities": []},
             {"text": "Nunca namorei", "intent": "negative", "entities": []},
-            {"text": "não, como faço para reconhecer?", "intent": "negative", "entities": []},
+            {
+                "text": "não, como faço para reconhecer?",
+                "intent": "negative",
+                "entities": [],
+            },
             {"text": "mais ou menos, pq?", "intent": "doubt", "entities": []},
-            {"text": "já mas não foi muito bom", "intent": "affirmative", "entities": []},
+            {
+                "text": "já mas não foi muito bom",
+                "intent": "affirmative",
+                "entities": [],
+            },
             {"text": "tudo ótimo", "intent": "affirmative", "entities": []},
             {"text": "tudo otimo", "intent": "affirmative", "entities": []},
             {"text": "tudo", "intent": "affirmative", "entities": []},
@@ -111,14 +148,30 @@ class TestEvaluateTask(unittest.TestCase):
             {"text": "tudo uma merda", "intent": "negative", "entities": []},
             {"text": "tudo horrivel", "intent": "negative", "entities": []},
             {"text": "tudo pessimo", "intent": "negative", "entities": []},
-            {"text": "eu também estou num relacionamento abusivo", "intent": "bias", "entities": []},
+            {
+                "text": "eu também estou num relacionamento abusivo",
+                "intent": "bias",
+                "entities": [],
+            },
             {"text": "já", "intent": "affirmative", "entities": []},
-            {"text": "hoje já estou num relacionamento abusivo", "intent": "bias", "entities": []},
-            {"text": "hoje estou num relacionamento abusivo", "intent": "bias", "entities": []},
+            {
+                "text": "hoje já estou num relacionamento abusivo",
+                "intent": "bias",
+                "entities": [],
+            },
+            {
+                "text": "hoje estou num relacionamento abusivo",
+                "intent": "bias",
+                "entities": [],
+            },
             {"text": "nunca passei por isso", "intent": "negative", "entities": []},
             {"text": "as vezes", "intent": "doubt", "entities": []},
             {"text": "sofro abuso emocional", "intent": "bias", "entities": []},
-            {"text": "to naum... mas ja namorei um porquinho?", "intent": "negative", "entities": []},
+            {
+                "text": "to naum... mas ja namorei um porquinho?",
+                "intent": "negative",
+                "entities": [],
+            },
             {"text": "estou namorando", "intent": "affirmative", "entities": []},
             {"text": "to namorando", "intent": "affirmative", "entities": []},
             {"text": "pior que ja", "intent": "affirmative", "entities": []},
@@ -126,8 +179,16 @@ class TestEvaluateTask(unittest.TestCase):
             {"text": "não entendi", "intent": "doubt", "entities": []},
             {"text": "eu quero", "intent": "affirmative", "entities": []},
             {"text": "gosto de futebol", "intent": "bias", "entities": []},
-            {"text": "meu namorado bateu na minha cara", "intent": "bias", "entities": []},
-            {"text": "não ne!! meu namorado bateu na minha cara", "intent": "bias", "entities": []},
+            {
+                "text": "meu namorado bateu na minha cara",
+                "intent": "bias",
+                "entities": [],
+            },
+            {
+                "text": "não ne!! meu namorado bateu na minha cara",
+                "intent": "bias",
+                "entities": [],
+            },
             {"text": "eu fui estruprada", "intent": "affirmative", "entities": []},
             {"text": "tenho que pensar", "intent": "doubt", "entities": []},
             {"text": "mais ou menos", "intent": "doubt", "entities": []},
@@ -146,22 +207,16 @@ class TestEvaluateTask(unittest.TestCase):
         ],
     )
     @patch(
-        "bothub_backend.bothub.BothubBackend.request_backend_parse_nlu",
-        return_value={
-            "version_id": 6647,
-            "total_training_end": 4,
-            "language": "pt_br",
-            "repository_uuid": "e1e8a0fa-625c-4ba3-8b91-4c9f308db791"
-        },
-    )
-    @patch(
         "bothub_backend.bothub.BothubBackend.request_backend_parse_nlu_persistor",
         return_value={
             "version_id": 49,
             "repository_uuid": "0f6b9644-db55-49a2-a20d-2af74106d892",
             "total_training_end": 3,
             "language": "pt_br",
-            "bot_data": base64.b64encode(open('example_bert_pt_br.tar.gz', 'rb').read())},
+            "bot_data": base64.b64encode(
+                open("example_generic_language.tar.gz", "rb").read()
+            ),
+        },
     )
     @patch(
         "bothub_backend.bothub.BothubBackend.request_backend_create_evaluate_results",
