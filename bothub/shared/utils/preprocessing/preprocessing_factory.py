43cccc757fa150f46e12812d7996ce6c98ca33ec
Lucas Agra <lucasomena@hotmail.com>
2021-02-24 16:57:56 -0300

    added spanish preprocessing/emoji

--- bothub/shared/utils/preprocessing/preprocessing_factory.py
+++ bothub/shared/utils/preprocessing/preprocessing_factory.py
@@ -1,6 +1,7 @@
 import logging
 from bothub.shared.utils.preprocessing.preprocessing_english import PreprocessingEnglish
 from bothub.shared.utils.preprocessing.preprocessing_portuguese import PreprocessingPortuguese
+from bothub.shared.utils.preprocessing.preprocessing_spanish import PreprocessingSpanish
 from bothub.shared.utils.preprocessing.preprocessing_base import PreprocessingBase
 
 logger = logging.getLogger(__name__)
@@ -22,6 +23,8 @@ class PreprocessingFactory(object):
                 return PreprocessingEnglish(self.remove_accent)
             elif self.language == "pt_br":
                 return PreprocessingPortuguese(self.remove_accent)
+            elif self.language == "es":
+                return PreprocessingSpanish(self.remove_accent)
             else:
                 return PreprocessingBase(self.remove_accent)
 
