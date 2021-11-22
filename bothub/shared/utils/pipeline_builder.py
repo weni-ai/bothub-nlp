2a0a7c04098e8cc9d1de590c7239d598f9bb4332
Lucas Agra <lucasomena@hotmail.com>
2021-11-22 17:05:15 -0300

    Introducing dynamic epochs

--- bothub/shared/utils/pipeline_builder.py
+++ bothub/shared/utils/pipeline_builder.py
@@ -1,3 +1,5 @@
+from typing import List, Callable
+
 from bothub.shared.utils.helpers import ALGORITHM_TO_LANGUAGE_MODEL
 from bothub_nlp_celery import settings
 from bothub.shared.utils.rasa_components.registry import language_to_model
@@ -9,16 +11,19 @@ class PipelineBuilder:
         self.language = update.get("language")
         self.algorithm = update.get("algorithm")
         self.use_name_entities = update.get("use_name_entities")
+        self.dataset_size = update.get("dataset_size")
         self.use_competing_intents = update.get("use_competing_intents")
         self.use_analyze_char = update.get("use_analyze_char")
         self.prebuilt_entities = update.get("prebuilt_entities", [])
         self.model = self._build_model_requirements()
         self.pipeline = self._build_pipeline()
 
-    def _add_spacy_nlp(self):
+    @staticmethod
+    def _add_spacy_nlp():
         return {"name": "bothub.shared.utils.pipeline_components.spacy_nlp.SpacyNLP"}
 
-    def _add_whitespace_tokenizer(self):
+    @staticmethod
+    def _add_whitespace_tokenizer():
         return {"name": "WhitespaceTokenizer"}
 
     def _add_preprocessing(self):
@@ -27,7 +32,8 @@ class PipelineBuilder:
             "language": self.language,
         }
 
-    def _add_regex_entity_extractor(self):
+    @staticmethod
+    def _add_regex_entity_extractor():
         return {
             "name": "bothub.shared.utils.pipeline_components.regex_entity_extractor.RegexEntityExtractorCustom"
         }
@@ -69,7 +75,8 @@ class PipelineBuilder:
             "language": self.language,
         }
 
-    def _add_embedding_intent_classifier(self):
+    @staticmethod
+    def _add_embedding_intent_classifier():
         return {
             "name": "bothub.shared.utils.pipeline_components.diet_classifier.DIETClassifierCustom",
             "hidden_layers_sizes": {"text": [256, 128]},
@@ -81,7 +88,60 @@ class PipelineBuilder:
             "BILOU_flag": False,
         }
 
-    def _add_diet_classifier(self, epochs=300, bert=False):
+    @staticmethod
+    def _epoch_factor_function1(examples_qnt: int, min_threshold: int = 10000) -> float:
+        """
+        :param examples_qnt: Number of examples in dataset
+        :param min_threshold: Minimum number of examples needed to have a factor > 1
+        :return: Division factor of defined maximum epochs
+
+        Example:
+        min_threshold = 10000
+          examples_qnt = 10000  -> (10*(10000-10000) + 100*10000)//10000   = 100 -> 100/100 = 1.0 (base case)
+          examples_qnt = 11000  -> (10*(11000-10000) + 100*10000)//11000   =  91 -> 100/91  = 1.09
+          examples_qnt = 15000  -> (10*(15000-10000) + 100*10000)//15000   =  70 -> 100/70  = 1,42
+          examples_qnt = 30000  -> (10*(30000-10000) + 100*10000)//30000   =  40 -> 100/40  = 2,5
+          examples_qnt = 33000  -> (10*(33000-10000) + 100*10000)//33000   =  37 -> 100/37  = 2,7
+          examples_qnt = 50000  -> (10*(50000-10000) + 100*10000)//50000   =  28 -> 100/28  = 3,57
+          examples_qnt = 100000 -> (10*(100000-10000) + 100*10000)//100000 =  19 -> 100/19  = 5,26
+
+        min_threshold = 5000
+          examples_qnt = 5000  -> (10*(5000-5000) + 100*5000)//5000   = 100 -> 100/100 = 1.0 (base case)
+          examples_qnt = 10000 -> (10*(10000-5000) + 100*5000)//10000 = 55  -> 100/55  = 1.81
+          examples_qnt = 15000 -> (10*(15000-5000) + 100*5000)//15000 = 40  -> 100/40  = 2,5
+
+        """
+        if examples_qnt <= min_threshold:
+            return 1.0
+
+        over_qnt = examples_qnt - min_threshold
+        epochs_fraction = ((10*over_qnt) + (100*min_threshold)) // examples_qnt
+        factor = 100 / epochs_fraction
+
+        return factor
+
+    def _calculate_epochs_number(
+        self,
+        max_epochs: int,
+        factor_function: Callable[[int, int], float]
+    ) -> int:
+        """
+        :param max_epochs: Maximum number of epochs to be considered
+        :param factor_function: Function that returns the division factor
+        :return: Calculated number of epochs (max_epochs/calculated_factor)
+        """
+        min_threshold = 10000
+
+        if self.dataset_size < min_threshold:
+            return max_epochs
+
+        factor = factor_function(self.dataset_size, min_threshold)
+        epochs = int(max_epochs // factor)
+        return epochs
+
+    def _add_diet_classifier(self, max_epochs=300, bert=False):
+        epochs = self._calculate_epochs_number(max_epochs, self._epoch_factor_function1)
+
         model = {
             "name": "bothub.shared.utils.pipeline_components.diet_classifier.DIETClassifierCustom",
             "entity_recognition": True,
@@ -121,7 +181,7 @@ class PipelineBuilder:
             self._add_countvectors_featurizer()
         )  # Bag of Words Featurizer
         partial_pipeline.append(
-            self._add_diet_classifier(epochs=150)
+            self._add_diet_classifier(max_epochs=150)
         )  # Intent Classifier
 
         return partial_pipeline
@@ -135,7 +195,7 @@ class PipelineBuilder:
             self._add_countvectors_featurizer()
         )  # Bag of Words Featurizer
         partial_pipeline.append(
-            self._add_diet_classifier(epochs=200)
+            self._add_diet_classifier(max_epochs=200)
         )  # Intent Classifier
 
         return partial_pipeline
@@ -163,7 +223,7 @@ class PipelineBuilder:
             self._add_countvectors_featurizer()
         )  # Bag of Words Featurizers
         partial_pipeline.append(
-            self._add_diet_classifier(epochs=100, bert=True)
+            self._add_diet_classifier(max_epochs=100, bert=True)
         )  # Intent Classifier
 
         return partial_pipeline
